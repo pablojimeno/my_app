74f187aff26e53a5458a13b7e8c9ea20ef6b4a76
Rober <proyectos@sonicbyte.com>
2011-03-11 13:58:04 +0100

    Integration tests for signing in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,13 @@ class UsersController < ApplicationController
   end
   
   def create
-    #raise params[:user].inspect
     @user = User.new(params[:user])
-      if @user.save
-        redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
-      else
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
       @title = "Sign up"
-      @user.password = ""
-      @user.password_confirmation = ""
       render 'new'
     end
   end
